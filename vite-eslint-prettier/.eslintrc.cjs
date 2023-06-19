eb40f6deb200ca17b3f0004d588044f2004e1027
Caio Barilli <caiobarilli@gmail.com>
2023-06-19 10:12:28 -0300

    add vite project

--- vite-eslint-prettier/.eslintrc.cjs
+++ vite-eslint-prettier/.eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh', 'prettier'],
   rules: {
+    'react-refresh/only-export-components': 'warn',
     'prettier/prettier': 'error',
   },
 }
